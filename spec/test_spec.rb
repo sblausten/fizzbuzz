754734af19e579cc386637321ceeb4c615373e32
sblausten <sblausten@gmail.com>
2017-04-05 16:12:44 +0100

    all other cases tests

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -29,7 +29,7 @@ describe 'fizzbuzz' do
 	  it 'returns "buzz" when passed 10' do
 	    expect(fizzbuzz(10)).to eq 'buzz'
 	  end
-	  it 'returns "buzz" when random multiple of 5' do
+	  it 'returns "buzz" when random integer multiple of 5' do
 	    expect(fizzbuzz(5*srand)).to eq 'buzz'
 	  end
 	end
@@ -39,8 +39,21 @@ describe 'fizzbuzz' do
 		it 'returns "fizzbuzz" when passed 15' do
 			expect(fizzbuzz(15)).to eq 'fizzbuzz'
 		end
-		it 'returns "fizzbuzz" when passed 15' do
+		it 'returns "fizzbuzz" when passed random integer multiple of 3 and 5' do
 			expect(fizzbuzz(5*3*srand)).to eq 'fizzbuzz'
+		end
+	end
+
+	describe "In all other cases, the program simply returns the given number." do
+		it "returns 4 when passed 4" do
+			expect(fizzbuzz(4)).to eq 4
+		end
+		it "returns given float when random float passed" do
+			x = rand
+			expect(fizzbuzz(x)).to eq x
+		end
+		it "returns 0 when 0 passed" do
+			expect(fizzbuzz(0)).to eq 0
 		end
 	end
 
