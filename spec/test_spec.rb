577b8352c7c4c106458fc57b79d3afca7583f662
sblausten <sblausten@gmail.com>
2017-04-05 15:29:53 +0100

    tests for multiples of three

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -9,7 +9,18 @@ In all other cases, the program simply returns the given number.
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
+
+	describe "When passed a number that is a multiple of 3, the program returns the message 'Fizz'." do 
+	  it 'returns "fizz" when passed 3' do
+	    expect(fizzbuzz(3)).to eq 'fizz'
+	  end
+	  it 'returns "fizz" when passed 9' do
+	    expect(fizzbuzz(9)).to eq 'fizz'
+	  end
+	  it 'returns "fizz" when passed 5' do
+	    expect(fizzbuzz(5)).to eq nil
+	  end
+	end
+
 end
+
