66181cee867403cde6ea815aabbe9b898d0d25df
sblausten <sblausten@gmail.com>
2017-04-05 15:20:03 +0100

    first basic test

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -6,4 +6,10 @@ When passed a number that is a multiple of both 3 and 5, the program ignores the
 In all other cases, the program simply returns the given number.
 =end
 
+require 'fizzbuzz'
 
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
